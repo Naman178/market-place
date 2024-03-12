d9822da4972694d61b5b16836ebf2dfd4fcf3fbb
Richa <richa.parekh@pavansgroup.in>
2024-03-12 23:50:48 +0530

    Home Page With 5 Section

---
+++ resources/views/home-page/common/header.blade.php
@@ -0,0 +1,30 @@
+<div class="header-container">
+    <div class="header-row">
+        <div class="col">
+            <div class="logo-container">
+                <a href="#">
+                    <img src="{{ asset('home-page/images/header_logo.png') }}" alt="Logo">
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <div class="menu-container">
+                <ul>
+                    <li><a href="#">Price</a></li>
+                    <li><a href="#">Documentation</a></li>
+                    <li><a href="#">Guide</a></li>
+                    <li><a href="#">Faq</a></li>
+                    <li><a href="#">Status</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col">
+            <div class="signin-container">
+                <ul>
+                    <li><a href="#">Login</a></li>
+                    <li class="signup-wrapper"><a href="#">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
