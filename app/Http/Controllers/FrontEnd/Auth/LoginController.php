829c5677e5344bf3bcf1b88c15c247ee52e42ef2
Richa <richa.parekh@pavansgroup.in>
2024-03-22 23:19:47 +0530

    Login with google

---
+++ app/Http/Controllers/FrontEnd/Auth/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\FrontEnd\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('front-end.auth.login');
+        //return Socialite::driver('google')->redirect();
+    }
+
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $user = Socialite::driver('google')->user();
+
+        // Check if the user exists in your database, or create a new user
+        $existingUser = User::where('email', $user->email)->first();
+
+        if ($existingUser) {
+            Auth::login($existingUser);
+        } else {
+            // Create new user
+            $newUser = new User();
+            $newUser->name = $user->name;
+            $newUser->email = $user->email;
+            $newUser->save();
+
+            Auth::login($newUser);
+        }
+
+        // Redirect to dashboard or desired route
+        return redirect()->route('home');
+    }
+}
