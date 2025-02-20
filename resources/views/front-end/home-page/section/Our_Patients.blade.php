1be10e16664858436d4714a8f2001bc4d8d7b0c3
shubham123350 <shubham.infinitysoftech@gmail.com>
2025-02-20 18:44:39 +0530

    adding dynamic site name and year

--- resources/views/front-end/home-page/section/Our_Patients.blade.php
+++ resources/views/front-end/home-page/section/Our_Patients.blade.php
@@ -5,15 +5,19 @@
                 <img class="mt-65" src="{{ asset('front-end/images/Group 5685.png') }}" alt="not found">
                 <h1>Our <span>Patients</span></h1>
                 <img class="vector2_img" src="{{ asset('front-end/images/Vector 8.png') }}" alt="not found">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-6 col-lg-12 col-sm-12 col-12 patientsSlider" id="patientsSlider">
                 <div>
-                    <img id="large-image" class="mt-65 rectangle_img" src="{{ asset('front-end/images/Rectangle 3985.png') }}" alt="not found">
+                    <img id="large-image" class="mt-65 rectangle_img" src="{{ asset('front-end/images/Rectangle 3985.png') }}" alt="not found" style="min-width: 80%;">
                 </div>
             </div>
-            <div class="col-xl-6 col-lg-12 col-sm-12 col-12 mt-13">
+            <div class="col-xl-6 col-lg-12 col-sm-12 col-12 mt-2">
                 <div class="">
                     <div class="testimonial-section">
                         <div class="testimonial-slider">
-                            <div class="testimonial-content">
+                            <div class="testimonial-content patientsSlider" id="patientsSlider">
                                 <p class="integra_p mt-65 mb-60 testimonial-text">
                                     Laoreet per malesuada montes lorem tincidunt id natoque parturient suspendisse
                                     senectus a
